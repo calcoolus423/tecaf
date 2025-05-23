8fdad68a59f948c530245d5d15f1b81dc5c9c604
Emiah Carpenter <emiahtc423@gmail.com>
2025-05-23 04:15:24 -0500

    commit before pull

--- main.cpp
+++ main.cpp
@@ -1,37 +1,61 @@
 #include "expression.hpp"
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 
-using std::cout;
 using std::ifstream;
 using std::setw;
 
-constexpr unsigned short COLUMN_WIDTH = 20;
+constexpr unsigned short COLUMN_WIDTH = 21;
 
 int main()
 {
 	bool result;
 	ifstream infile("prog1Data.txt");
 	string expression;
+	string prefix;
 	string postfix;
 
+	boolExp* myExp;
+
 	// header
-	cout << setw(COLUMN_WIDTH + 3) << std::left << "Input";
+	cout << setw(static_cast<unsigned short>(COLUMN_WIDTH + 3)) << std::left << "Original (Infix)";
+	cout << setw(static_cast<unsigned short>(COLUMN_WIDTH + 3)) << std::left << "Postfix";
+	cout << setw(static_cast<unsigned short>(COLUMN_WIDTH + 3)) << std::left << "Prefix";
+	// cout << setw(static_cast<unsigned short>(COLUMN_WIDTH + 3)) << std::left << "Prefix";
 	cout << setw(COLUMN_WIDTH) << std::left << "Output" << '\n';
 
 	// get the expression
 	while (!infile.eof())
 	{
+		// get the expression to evaluate in infix notation
 		std::getline(infile, expression);
 
+		// create a new boolean expression object
+		myExp = new boolExp(expression);
+
+		// write the original expression
 		cout << setw(COLUMN_WIDTH) << std::right << expression << " | ";
 
-		postfix = infix_to_postfix_bool(expression);
-		result = eval_boolExp(postfix);
+		// find the result
+		result = myExp->evaluate();
 
+		postfix = myExp->getExpression("postfix");
+
+		cout << setw(COLUMN_WIDTH) << std::right << postfix << " | ";
+
+		prefix = myExp->getExpression("prefix");
+
+		cout << setw(COLUMN_WIDTH) << std::right << prefix << " | ";
+
+		// print the result
 		cout << setw(COLUMN_WIDTH) << std::right << result << '\n';
+
+		myExp->
+
+		delete myExp;
 	}
+
+	myExp = nullptr;
 
 	return 0;
 }
